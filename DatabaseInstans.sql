ce529724c3cc5b29371f06bfb6472b76ef40d819
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2022-04-06 10:50:30 +0200

    Add files via upload

--- DatabaseInstans.sql
+++ DatabaseInstans.sql
@@ -71,6 +71,10 @@ INSERT INTO Person(Email, Fornavn, Efternavn, Adresse, Postnr, Fødselsdato, Kø
 ('knaldperlen@gmail.com', 'Brian', 'Briansen', 'Briansvej', '1500', '19900202', 'M'),
 ('DanseMyggen@gmail.com', 'Karen', 'Briansen', 'Briansvej', '1500', '19900201', 'K');
 
+INSERT INTO EventType(EventTypeID) VALUES
+('MTB'),
+('10km');
+
 INSERT INTO Begivenhed(ForeningsID, Dato, EventTypeID) VALUES
 ('oa', '20220330', 'MTB'),
 ('ua', '20220202', '10km');
@@ -80,17 +84,50 @@ INSERT INTO Deltager(Email, ForeningsID, Dato, EventTypeID, StartNr, Resultat) V
 ('DanseMyggen@gmail.com', 'ua', '20220202', '10km', '001', '00:30:28');
 
 INSERT INTO Aldersklasse(EventTypeID, Køn, FraAlder, TilAlder) VALUES
-('0','9'),
-('10','19'),
-('20','29'),
-('30','39'),
-('40','49'),
-('50','59'),
-('60','69'),
-('70','79'),
-('80','89'),
-('90','99'),
-('100','199');
+('MTB','M','0','9'),
+('MTB','M','10','19'),
+('MTB','M','20','29'),
+('MTB','M','30','39'),
+('MTB','M','40','49'),
+('MTB','M','50','59'),
+('MTB','M','60','69'),
+('MTB','M','70','79'),
+('MTB','M','80','89'),
+('MTB','M','90','99'),
+('MTB','M','100','199'),
+('MTB','K','0','9'),
+('MTB','K','10','19'),
+('MTB','K','20','29'),
+('MTB','K','30','39'),
+('MTB','K','40','49'),
+('MTB','K','50','59'),
+('MTB','K','60','69'),
+('MTB','K','70','79'),
+('MTB','K','80','89'),
+('MTB','K','90','99'),
+('MTB','K','100','199'),
+('10km','M','0','9'),
+('10km','M','10','19'),
+('10km','M','20','29'),
+('10km','M','30','39'),
+('10km','M','40','49'),
+('10km','M','50','59'),
+('10km','M','60','69'),
+('10km','M','70','79'),
+('10km','M','80','89'),
+('10km','M','90','99'),
+('10km','M','100','199'),
+('10km','K','0','9'),
+('10km','K','10','19'),
+('10km','K','20','29'),
+('10km','K','30','39'),
+('10km','K','40','49'),
+('10km','K','50','59'),
+('10km','K','60','69'),
+('10km','K','70','79'),
+('10km','K','80','89'),
+('10km','K','90','99'),
+('10km','K','100','199');
 
 # Vis tabellerne
 SELECT * FROM Idrætsforening;
