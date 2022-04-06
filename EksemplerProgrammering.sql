be04887cab96ef9097d8c3f4515dc638be0eb445
Xiao <91072285+Xiao2305@users.noreply.github.com>
2022-04-06 19:31:35 +0300

    Add files via upload

--- EksemplerProgrammering.sql
+++ EksemplerProgrammering.sql
@@ -1,32 +1,33 @@
+USE Tidsmaskinen;
 USE DELIMITER //
-
-CREATE FUNCTION TotalDeltagerAntalPrForening (vID VARCHAR(10)) RETURNS INT
+CREATE FUNCTION TotalDeltagerAntalPrForening (vForeningsID VARCHAR(10)) RETURNS INT
 BEGIN
 	DECLARE vDeltagere INT;
     SELECT COUNT(*) INTO vDeltagere FROM Deltager
-    WHERE ID = vID;
+    WHERE ForeningsID = vForeningsID;
     RETURN vDeltagere;
 END //
 
 
-CREATE FUNCTION ProcentKvinderPrEventType (vEventType VARCHAR(35)) RETURNS INT
+CREATE FUNCTION ProcentKvinderPrEventType (vEventTypeID VARCHAR(35)) RETURNS INT
 BEGIN
 	DECLARE vAntalKvinder INT;
     DECLARE vTotalAntalDeltagere INT;
     SELECT COUNT(*) INTO vTotalAntalDeltagere FROM Deltager
-    WHERE EventType = vEventType;
+    WHERE EventTypeID = vEventTypeID;
     SELECT COUNT(*) INTO vAntalKvinder FROM Deltager NATURAL JOIN Person
-    WHERE EventType = vEventType AND Køn = 'K';
+    WHERE EventTypeID = vEventTypeID AND Køn = 'K';
     RETURN vAntalKvinder / vTotalAntalDeltagere * 100;
 END //
 
 
-CREATE PROCEDURE TilmeldDeltagerOgTildelStartNr (IN vEmail VARCHAR(35), IN vID VARCHAR(10), IN vDato DATE, IN vEventType VARCHAR(35))
+CREATE PROCEDURE TilmeldDeltagerOgTildelStartNr (IN vEmail VARCHAR(35), IN vForeningsID VARCHAR(10), 
+IN vDato DATE, IN vEventTypeID VARCHAR(35))
 BEGIN
 	DECLARE vmaxStartNr INT; 
 	SELECT MAX(StartNr) INTO vmaxStartNr FROM Deltager
-    WHERE ID = vID AND Dato = vDato AND EventType = vEventType;
-    INSERT Deltager VALUES (vEmail, vID, vDato, vEventType, (vmaxStartNr+1), null);
+    WHERE ForeningsID = vForeningsID AND Dato = vDato AND EventTypeID = vEventTypeID;
+    INSERT Deltager VALUES (vEmail, vForeningsID, vDato, vEventTypeID, (vmaxStartNr+1), null);
 END //
 
 
@@ -37,7 +38,7 @@ BEGIN
     IF vCount = 0 THEN
 		START TRANSACTION;
 		INSERT INTO Person
-        SELECT vNyEmail, Fornavn, Efternavn, Vej, Fødselsdato, Køn FROM Person
+        SELECT vNyEmail, Fornavn, Efternavn, Adresse, Postnr, Fødselsdato, Køn FROM Person
         WHERE Email = vGammelEmail;
 		UPDATE Deltager SET Email = vNyEmail WHERE Email = vGammelEmail;
 		SELECT COUNT(*) INTO vCount FROM Deltager WHERE Email = vGammelEmail;
@@ -62,14 +63,14 @@ END //
 USE DELIMITER ;
 
 # eksempler på brug af funktioner / procedurer
-SELECT Navn, TotalDeltagerAntalPrForening(ID) as TotalAntalEventDeltagere FROM Idrætsforening;
-SELECT EventType, ProcentKvinderPrEventType(EventType) as ProcentAndelKvinder FROM Begivenhed;
-CALL TilmeldDeltagerOgTildelStartNr('DanseMyggen@gmail.com','OverAc', '20220330', 'MTB');
+SELECT Navn, TotalDeltagerAntalPrForening(ForeningsID) as TotalAntalEventDeltagere FROM Idrætsforening;
+SELECT EventTypeID, ProcentKvinderPrEventType(EventTypeID) as ProcentAndelKvinder FROM Begivenhed;
+CALL TilmeldDeltagerOgTildelStartNr('DanseMyggen@gmail.com','O', '20220330', 'MTB');
 CALL ÆndreEmail('DanseMyggen@gmail.com', 'DanceMonkey@tones.com');
-INSERT Person VALUES ('ikkegyldig.dk', 'Karen', 'Briansen', 'Briansvej', '19900201', 'K');
+INSERT Person VALUES ('ikkegyldig.dk', 'Karen', 'Briansen', 'Briansvej','2860', '19900201', 'K');
 
 SELECT * FROM Deltager;
 SELECT * FROM Idrætsforening;
 SELECT * FROM Begivenhed;
 SELECT * FROM Person;
-SELECT * FROM aldersklasse;
+SELECT * FROM EventType;
